e2f936c1e1c233347a2b7b525e72957a8b6e7cf1
Shal1928 <Shal1928@gmail.com>
2012-02-22 04:13:42 +0000

    Add Controls, ExtermalLibraries, Visual

---
+++ UseAbilities/UseAbilities.Controls/Dialogs/DependencyFolderBrowseDialog.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using UseAbilities.MVVM.Command;
+using Control = System.Windows.Controls.Control;
+
+namespace UseAbilities.Controls.Dialogs
+{
+    [DesignTimeVisible(false)]
+    public class DependencyFolderBrowseDialog : Control
+    {
+        public DependencyFolderBrowseDialog()
+        {
+            Visibility = Visibility.Collapsed;
+        }
+
+        private FolderBrowserDialog _dialog;
+        private FolderBrowserDialog Dialog
+        {
+            get
+            {
+                return _dialog ?? (_dialog = new FolderBrowserDialog());
+            }
+        }
+        
+
+        #region Commands
+
+        public static readonly DependencyProperty FolderOkCommandProperty =
+            DependencyProperty.Register("FolderOkCommand", typeof(ICommand), typeof(DependencyFolderBrowseDialog));
+
+        public ICommand FolderOkCommand
+        {
+            get
+            {
+                return (ICommand)GetValue(FolderOkCommandProperty);
+            }
+            set
+            {
+                SetValue(FolderOkCommandProperty, value);
+            }
+        }
+
+
+        private ICommand _showDialogCommand;
+        public ICommand ShowDialogCommand
+        {
+            get
+            {
+                return _showDialogCommand ?? (_showDialogCommand = new RelayCommand(p => ShowDialog()));
+            }
+        }
+
+        private void ShowDialog()
+        {
+            if(Dialog.ShowDialog()!=DialogResult.OK) return;
+
+            if (FolderOkCommand != null) FolderOkCommand.Execute(Dialog.SelectedPath);
+        }
+
+        #endregion
+
+
+        #region Dependency Properties
+
+        #region Description
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(DependencyFolderBrowseDialog),
+            new UIPropertyMetadata("Select a folder", DialogPropertyChangedCallback));
+
+        public string Description
+        {
+            get
+            {
+                return (string)GetValue(DescriptionProperty);
+            }
+            set
+            {
+                SetValue(DescriptionProperty, value);
+            }
+        }
+
+        #endregion
+
+        #region ShowNewFolderButton
+
+        public static readonly DependencyProperty ShowNewFolderButtonProperty =
+            DependencyProperty.Register("ShowNewFolderButton", typeof(bool), typeof(DependencyFolderBrowseDialog),
+            new UIPropertyMetadata(true, DialogPropertyChangedCallback));
+
+        public bool ShowNewFolderButton
+        {
+            get
+            {
+                return (bool)GetValue(ShowNewFolderButtonProperty);
+            }
+            set
+            {
+                SetValue(ShowNewFolderButtonProperty, value);
+            }
+        }
+
+        #endregion
+
+        #region RootFolder
+
+        public static readonly DependencyProperty RootFolderProperty =
+            DependencyProperty.Register("SelectedPath", typeof(Environment.SpecialFolder), typeof(DependencyFolderBrowseDialog),
+            new UIPropertyMetadata(Environment.SpecialFolder.Desktop, DialogPropertyChangedCallback));
+
+        public Environment.SpecialFolder RootFolder
+        {
+            get
+            {
+                return (Environment.SpecialFolder)GetValue(RootFolderProperty);
+            }
+            set
+            {
+                SetValue(RootFolderProperty, value);
+            }
+        }
+
+        #endregion
+
+        private static void DialogPropertyChangedCallback(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var dfd = obj as DependencyFolderBrowseDialog;
+            if (dfd == null) return;
+
+            var changedProp = dfd.Dialog.GetType().GetProperty(args.Property.Name);
+            if (changedProp.CanWrite) changedProp.SetValue(dfd.Dialog, args.NewValue, null);
+        }
+
+        #endregion
+    }
+}
---
+++ UseAbilities/UseAbilities.Controls/Dialogs/DependencyOpenFileDialog.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using UseAbilities.Controls.Dialogs.Base;
+
+namespace UseAbilities.Controls.Dialogs
+{
+    public class DependencyOpenFileDialog : DependencyFileDialog
+    {
+        private OpenFileDialog _dialog;
+
+        /// <summary>
+        /// Overridden from DependencyFileDialog. Provides access to an instance of the Microsoft.Win32.OpenFileDialog class.
+        /// </summary>
+        protected override FileDialog Dialog
+        {
+            get
+            {
+                return _dialog ?? (_dialog = new OpenFileDialog());
+            }
+        }
+
+        /// <summary>
+        /// Overridden from DependencyFileDialog. Provides support for the MultiSelect property. Smells fragile.
+        /// </summary>
+        protected override void ExecuteFileOkCommand()
+        {
+            if (MultiSelect)
+            {
+                FileOkCommand.Execute(Dialog.FileNames);
+            }
+            else
+            {
+                base.ExecuteFileOkCommand();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets an option indicating whether OpenFileDialog allows users to select multiple files.
+        /// </summary>
+        public bool MultiSelect
+        {
+            get
+            {
+                return (bool)GetValue(MultiSelectProperty);
+            }
+            set
+            {
+                SetValue(MultiSelectProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// The Dependency Property for the MultiSelect property
+        /// </summary>
+        public static readonly DependencyProperty MultiSelectProperty =
+            DependencyProperty.Register("MultiSelect", typeof(bool), typeof(DependencyOpenFileDialog),
+            new UIPropertyMetadata(false, DialogPropertyChangedCallback));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the read-only check box displayed by DependencyOpenFileDialog is selected. 
+        /// </summary>
+        public bool ReadOnlyChecked
+        {
+            get
+            {
+                return (bool)GetValue(ReadOnlyCheckedProperty);
+            }
+            set
+            {
+                SetValue(ReadOnlyCheckedProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// The Dependency Property for the ReadOnlyChecked property
+        /// </summary>
+        public static readonly DependencyProperty ReadOnlyCheckedProperty =
+            DependencyProperty.Register("ReadOnlyChecked", typeof(bool), typeof(DependencyOpenFileDialog),
+            new UIPropertyMetadata(false, DialogPropertyChangedCallback));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether DependencyOpenFileDialog contains a read-only check box.
+        /// </summary>
+        public bool ShowReadOnly
+        {
+            get
+            {
+                return (bool)GetValue(ShowReadOnlyProperty);
+            }
+            set
+            {
+                SetValue(ShowReadOnlyProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// The Dependency Property for the ShowReadOnly property
+        /// </summary>
+        public static readonly DependencyProperty ShowReadOnlyProperty =
+            DependencyProperty.Register("ShowReadOnly", typeof(bool), typeof(DependencyOpenFileDialog),
+            new UIPropertyMetadata(false, DialogPropertyChangedCallback));
+    }
+}
