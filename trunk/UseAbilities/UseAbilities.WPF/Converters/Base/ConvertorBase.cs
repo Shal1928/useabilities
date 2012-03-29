fd4d3b5f6149d0ba7da7f2e53417ac65a9ddc4e8
shal1928 <shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2012-03-29 08:36:14 +0000

    Add Base Converter for simply markup (without declarating in resources)

---
+++ trunk/UseAbilities/UseAbilities.WPF/Converters/Base/ConvertorBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace UseAbilities.WPF.Converters.Base
+{
+    public abstract class ConvertorBase<T> : MarkupExtension, IValueConverter where T : class, new()
+    {
+        /// <summary>
+        /// Must be implemented in inheritor.
+        /// </summary>
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        /// <summary>
+        /// Override if needed.
+        /// </summary>
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region MarkupExtension members
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _converter ?? (_converter = new T());
+        }
+
+        private static T _converter;
+
+        #endregion
+    }
+
+    //<Label Content="{Binding Path=Date, Converter={converters:DateConverter}}" />
+}
