ac47de79d14596d36ae551be9fd2fd6e5c9cca10
shal1928 <shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2012-03-29 08:55:56 +0000

    Add MultiConverterBase and extract ConverterMarkupExtensionBase

---
+++ trunk/UseAbilities/UseAbilities.WPF/Converters/Base/ConverterMarkupExtensionBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace UseAbilities.WPF.Converters.Base
+{
+    public abstract class ConverterMarkupExtensionBase<T> : MarkupExtension where T : class, new()
+    {
+        #region MarkupExtension members
+
+        private static T _converter;
+        
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _converter ?? (_converter = new T());
+        }
+
+        #endregion
+    }
+}
