c38fb6f76d77126ba04a7da09ae6d820142f1228
shal1928 <shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2012-06-06 04:05:13 +0000

    Fix Names for DateTimeWeek extension & Add GetWeek

--- UseAbilities/UseAbilities.Extensions/DateTimeExt/DateTimeWeekExt.cs
+++ UseAbilities/UseAbilities.Extensions/DateTimeExt/DateTimeWeekExt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UseAbilities.Extensions.DateTimeExt
 {
@@ -14,14 +15,36 @@ namespace UseAbilities.Extensions.DateTimeExt
             return dt.AddDays(-1 * diff).Date;
         }
 
-        public static DateTime FirstDayLastWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
+        public static DateTime FirstDayOfWeekByDate(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
         {
-            return dt.AddDays(-7).StartOfWeek(startOfWeek);
+            return dt.StartOfWeek(startOfWeek);
         }
+
+        public static List<DateTime> GetWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
+        {
+            var week = new List<DateTime>();
+            for (var i = 0; i < 7; i++)
+                week.Add(dt.FirstDayOfWeekByDate(startOfWeek).AddDays(i));
+
+            return week;
+        }
+        
 
         public static DateTime FirstDayCurrentWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
         {
-            return dt.StartOfWeek(startOfWeek);
+            return DateTime.Now.StartOfWeek(startOfWeek);
+        }
+
+        public static DateTime FirstDayLastWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
+        {
+            return DateTime.Now.AddDays(-7).StartOfWeek(startOfWeek);
+        }
+
+
+
+        public static bool ThisDateCurrentWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
+        {
+            return dt.Date >= DateTime.Now.FirstDayCurrentWeek(startOfWeek).Date;
         }
 
         public static bool ThisDateLastWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
@@ -29,19 +52,15 @@ namespace UseAbilities.Extensions.DateTimeExt
             return dt.Date >= DateTime.Now.FirstDayLastWeek(startOfWeek).Date && dt.Date < DateTime.Now.FirstDayCurrentWeek(startOfWeek).Date;
         }
 
-        public static bool ThisDateCurrentWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
+
+        public static bool ThisDateToday(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
         {
-            return dt.Date >= DateTime.Now.FirstDayCurrentWeek(startOfWeek).Date;
+            return dt.Date == DateTime.Now.Date;
         }
 
         public static bool ThisDateYesterday(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
         {
             return dt.Date == DateTime.Now.AddDays(-1).Date;
-        }
-
-        public static bool ThisDateToday(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
-        {
-            return dt.Date == DateTime.Now.Date;
         }
     }
 }
