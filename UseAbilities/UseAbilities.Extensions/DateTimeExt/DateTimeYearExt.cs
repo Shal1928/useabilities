94911d1f2d72f7dab5a089d8b12013cc1c355a6b
shal1928 <shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2012-06-06 06:28:34 +0000

    Add GetQuarterByMonth, GetMonthes in to DateTimeYearExt

--- UseAbilities/UseAbilities.Extensions/DateTimeExt/DateTimeYearExt.cs
+++ UseAbilities/UseAbilities.Extensions/DateTimeExt/DateTimeYearExt.cs
@@ -1,12 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace UseAbilities.Extensions.DateTimeExt
 {
     public static class DateTimeYearExt
     {
+        public static List<DateTime> GetQuarterByMonth(this DateTime dt)
+        {
+            var year = dt.Year;
+            var month = dt.Month;
 
+            if (month <= 3) return new List<DateTime>
+                                          {
+                                              new DateTime(year, 1, 1),
+                                              new DateTime(year, 2, 1),
+                                              new DateTime(year, 3, 1)
+                                          };
+
+            if ((month >= 4) && (month <= 6)) return new List<DateTime>
+                                          {
+                                              new DateTime(year, 4, 1),
+                                              new DateTime(year, 5, 1),
+                                              new DateTime(year, 6, 1)
+                                          };
+
+            if ((month >= 7) && (month <= 9)) return new List<DateTime>
+                                          {
+                                              new DateTime(year, 7, 1),
+                                              new DateTime(year, 8, 1),
+                                              new DateTime(year, 9, 1)
+                                          };
+
+            if (month >= 10) return new List<DateTime>
+                                          {
+                                              new DateTime(year, 10, 1),
+                                              new DateTime(year, 11, 1),
+                                              new DateTime(year, 12, 1)
+                                          };
+
+            throw new Exception("A Month can not be greater than 12 and less than 1");
+        }
+
+        public static List<DateTime> GetMonth(this DateTime dt)
+        {
+            var year = dt.Year;
+
+            return new List<DateTime>
+                       {
+                           new DateTime(year, 1, 1),
+                           new DateTime(year, 2, 1),
+                           new DateTime(year, 3, 1),
+                           new DateTime(year, 4, 1),
+                           new DateTime(year, 5, 1),
+                           new DateTime(year, 6, 1),
+                           new DateTime(year, 7, 1),
+                           new DateTime(year, 8, 1),
+                           new DateTime(year, 9, 1),
+                           new DateTime(year, 10, 1),
+                           new DateTime(year, 11, 1),
+                           new DateTime(year, 12, 1)
+                       };
+        }
     }
 }
