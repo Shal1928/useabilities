02ef1a1ee74d26fc0e2a284393db7b65765363b3
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2012-08-27 16:12:06 +0000

    A: 1.0.0.0; F: 1.0.0.0;

--- trunk/UseAbilities/UseAbilities.Visual/Properties/AssemblyInfo.cs
+++ trunk/UseAbilities/UseAbilities.Visual/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UseAbilities.Visual")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Use wpf visual abilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UseAbilities.Visual")]
+[assembly: AssemblyProduct("UseAbilities")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
