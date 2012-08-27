f976eb22ff7772cd544150e4dcb7b1facf79e20f
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2012-08-27 16:10:19 +0000

    A: 1.0.0.0; F: 1.0.0.0

--- UseAbilities/UseAbilities.MVVM/Properties/AssemblyInfo.cs
+++ UseAbilities/UseAbilities.MVVM/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Common.MVVM.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("UseAbilities.MVVM")]
+[assembly: AssemblyDescription("Use model view viewmodel abilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Common.MVVM.Core")]
+[assembly: AssemblyProduct("UseAbilities")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
