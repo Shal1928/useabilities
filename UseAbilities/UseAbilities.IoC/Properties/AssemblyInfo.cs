37bf8b47296cd54eccc24b62b4bf53e7c1e9951e
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2012-08-27 16:08:59 +0000

    A: 1.0.0.0; F: 1.0.0.0

--- UseAbilities/UseAbilities.IoC/Properties/AssemblyInfo.cs
+++ UseAbilities/UseAbilities.IoC/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Common.Code.UseAbilities.IoC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("UseAbilities.IoC")]
+[assembly: AssemblyDescription("Use Inversion of control abilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Common.Code.UseAbilities.IoC")]
+[assembly: AssemblyProduct("UseAbilities")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
