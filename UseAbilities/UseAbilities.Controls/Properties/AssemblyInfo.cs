70f5f2ebc580813244dd7c2307a9e55a5657980d
shal1928 <shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2012-08-28 03:17:23 +0000

    A: 1.0.0.0; F: 1.0.0.0;

--- UseAbilities/UseAbilities.Controls/Properties/AssemblyInfo.cs
+++ UseAbilities/UseAbilities.Controls/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UseAbilities.Controls")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Use custom wpf controls abilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UseAbilities.Controls")]
+[assembly: AssemblyProduct("UseAbilities")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
