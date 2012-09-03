d4a7d1f9c04a4adf7d9fdf159e6b3c5e6606936e
shal1928 <shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2012-09-03 10:01:40 +0000

    A: 1.0.0.0; F: 1.0.1.0; Add Instance static class for create instances by type or constructorInfo;

---
+++ trunk/UseAbilities/UseAbilities.DotNet/Reflection/Instance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace UseAbilities.DotNet.Reflection
+{
+    public static class Instance
+    {
+        private const BindingFlags BINDING_FLAGS_ALL_INSTANCE_CTORS = BindingFlags.NonPublic |
+                                                                      BindingFlags.Public |
+                                                                      BindingFlags.Instance;
+
+        public static object Create(ConstructorInfo ctor, params object[] args)
+        {
+            return CreateInstance(ctor, args);
+        }
+        
+        public static object Create(Type type, params object[] args)
+        {
+            var ctor = type.GetConstructors(BINDING_FLAGS_ALL_INSTANCE_CTORS).First();
+            return CreateInstance(ctor, args);
+        }
+
+        private static object CreateInstance(ConstructorInfo ctor, params object[] args)
+        {
+            var newExp = Expression.New(ctor, args.Select(Expression.Constant));
+            var lambda = Expression.Lambda(typeof(Func<object>), newExp);
+            var compiled = (Func<object>)lambda.Compile();
+
+            var instance = compiled();
+            return instance;
+        }
+    }
+}
