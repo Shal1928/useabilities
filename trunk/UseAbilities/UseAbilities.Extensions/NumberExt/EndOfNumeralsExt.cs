c583ab84eb1bcd2f78b0a037c668fbac7002e565
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2012-09-15 13:28:39 +0000

    A: 1.0.0.0; F: 1.0.8.0; Add EndOfNumeralsExt

---
+++ trunk/UseAbilities/UseAbilities.Extensions/NumberExt/EndOfNumeralsExt.cs
@@ -0,0 +1,24 @@
+﻿namespace UseAbilities.Extensions.NumberExt
+{
+    public static class EndOfNumeralsExt
+    {
+        private const string Yi = "ый";
+        private const string Oi = "ой";
+        private const string Ii = "ий";
+
+        /// <summary>
+        /// Invalid rule for Russian language. Valid use only й letter
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static string GetAccretionOfCaseEndings(this int number)
+        {
+            var end = string.Empty;
+            if (number == 3)  end = Ii;
+            if (number == 2 || (number > 5 && number < 9)) end = Oi;
+            if (number == 1 || (number > 3 && number < 6) || number >= 9) end =  Yi;
+
+            return string.Format("{0}{1}",number, end);
+        }
+    }
+}
