bb278895fd6cc7af97f64d9abac7426ed2a08f77
Shal1928@gmail.com <Shal1928@gmail.com@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2013-01-11 02:07:07 +0000

    Fix CloneableObject for Properties

--- trunk/UseAbilities/UseAbilities.DotNet/Reflection/CloneableObject.cs
+++ trunk/UseAbilities/UseAbilities.DotNet/Reflection/CloneableObject.cs
@@ -9,64 +9,52 @@ namespace UseAbilities.DotNet.Reflection
         {
             var newObject = Activator.CreateInstance(GetType());
 
-            var fields = newObject.GetType().GetFields();
+            var properties = newObject.GetType().GetProperties();
 
             var i = 0;
 
-            foreach (var fieldInfo in GetType().GetFields())
+            foreach (var propertyInfo in GetType().GetProperties())
             {
-                var cloneType = fieldInfo.FieldType.GetInterface("ICloneable", true);
+                var cloneType = propertyInfo.PropertyType.GetInterface("ICloneable", true);
 
                 if (cloneType != null)
                 {
-                    var clone = (ICloneable)fieldInfo.GetValue(this);
-                    fields[i].SetValue(newObject, clone.Clone());
+                    var clone = (ICloneable)propertyInfo.GetValue(this, null);
+                    properties[i].SetValue(newObject, clone.Clone(), null);
                 }
-                else fields[i].SetValue(newObject, fieldInfo.GetValue(this));
+                else properties[i].SetValue(newObject, propertyInfo.GetValue(this, null), null);
                 
-                var enumerableType = fieldInfo.FieldType.GetInterface("IEnumerable", true);
+                var enumerableType = propertyInfo.PropertyType.GetInterface("IEnumerable", true);
                 if (enumerableType != null)
                 {
-                    var IEnum = (IEnumerable)fieldInfo.GetValue(this);
+                    var IEnum = (IEnumerable)propertyInfo.GetValue(this, null);
 
-                    var listType = fields[i].FieldType.GetInterface("IList", true);
-                    var dicType = fields[i].FieldType.GetInterface("IDictionary", true);
+                    var listType = properties[i].PropertyType.GetInterface("IList", true);
+                    var dicType = properties[i].PropertyType.GetInterface("IDictionary", true);
                     var j = 0;
 
                     if (listType != null)
                     {
-                        var list = (IList)fields[i].GetValue(newObject);
+                        var list = (IList)properties[i].GetValue(newObject, null);
 
                         foreach (var obj in IEnum)
                         {
                             cloneType = obj.GetType().GetInterface("ICloneable", true);
 
-                            if (cloneType != null)
-                            {
-                                var clone = (ICloneable)obj;
-                                list[j] = clone.Clone();
-                            }
-
+                            if (cloneType != null) list[j] = ((ICloneable)obj).Clone();
                             j++;
                         }
                     }
                     else if (dicType != null)
                     {
-                        var dic = (IDictionary)fields[i].GetValue(newObject);
+                        var dic = (IDictionary)properties[i].GetValue(newObject, null);
                         j = 0;
 
                         foreach (DictionaryEntry de in IEnum)
                         {
+                            cloneType = de.Value.GetType().GetInterface("ICloneable", true);
 
-                            cloneType = de.Value.GetType().
-                                GetInterface("ICloneable", true);
-
-                            if (cloneType != null)
-                            {
-                                var clone = (ICloneable)de.Value;
-
-                                dic[de.Key] = clone.Clone();
-                            }
+                            if (cloneType != null) dic[de.Key] = ((ICloneable)de.Value).Clone();
                             j++;
                         }
                     }
@@ -75,5 +63,76 @@ namespace UseAbilities.DotNet.Reflection
             }
             return newObject;
         }
+
+        //public object Clone()
+        //{
+        //    var newObject = Activator.CreateInstance(GetType());
+
+        //    var fields = newObject.GetType().GetFields();
+
+        //    var i = 0;
+
+        //    foreach (var fieldInfo in GetType().GetFields())
+        //    {
+        //        var cloneType = fieldInfo.FieldType.GetInterface("ICloneable", true);
+
+        //        if (cloneType != null)
+        //        {
+        //            var clone = (ICloneable)fieldInfo.GetValue(this);
+        //            fields[i].SetValue(newObject, clone.Clone());
+        //        }
+        //        else fields[i].SetValue(newObject, fieldInfo.GetValue(this));
+
+        //        var enumerableType = fieldInfo.FieldType.GetInterface("IEnumerable", true);
+        //        if (enumerableType != null)
+        //        {
+        //            var IEnum = (IEnumerable)fieldInfo.GetValue(this);
+
+        //            var listType = fields[i].FieldType.GetInterface("IList", true);
+        //            var dicType = fields[i].FieldType.GetInterface("IDictionary", true);
+        //            var j = 0;
+
+        //            if (listType != null)
+        //            {
+        //                var list = (IList)fields[i].GetValue(newObject);
+
+        //                foreach (var obj in IEnum)
+        //                {
+        //                    cloneType = obj.GetType().GetInterface("ICloneable", true);
+
+        //                    if (cloneType != null)
+        //                    {
+        //                        var clone = (ICloneable)obj;
+        //                        list[j] = clone.Clone();
+        //                    }
+
+        //                    j++;
+        //                }
+        //            }
+        //            else if (dicType != null)
+        //            {
+        //                var dic = (IDictionary)fields[i].GetValue(newObject);
+        //                j = 0;
+
+        //                foreach (DictionaryEntry de in IEnum)
+        //                {
+
+        //                    cloneType = de.Value.GetType().
+        //                        GetInterface("ICloneable", true);
+
+        //                    if (cloneType != null)
+        //                    {
+        //                        var clone = (ICloneable)de.Value;
+
+        //                        dic[de.Key] = clone.Clone();
+        //                    }
+        //                    j++;
+        //                }
+        //            }
+        //        }
+        //        i++;
+        //    }
+        //    return newObject;
+        //}
     }
 }
