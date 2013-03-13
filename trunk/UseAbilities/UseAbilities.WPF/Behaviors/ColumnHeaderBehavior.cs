411264e76d81c4679aede11305c5ebd86db4ddb6
Shal1928@gmail.com <Shal1928@gmail.com@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2013-03-13 05:40:46 +0000

    Revert to old version, without DataTemplate Properties

--- trunk/UseAbilities/UseAbilities.WPF/Behaviors/ColumnHeaderBehavior.cs
+++ trunk/UseAbilities/UseAbilities.WPF/Behaviors/ColumnHeaderBehavior.cs
@@ -1,12 +1,8 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Reflection;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Interactivity;
 using UseAbilities.WPF.Attributes;
-using UseAbilities.WPF.Controls.DataGrid;
 
 namespace UseAbilities.WPF.Behaviors
 { 
@@ -15,119 +11,12 @@ namespace UseAbilities.WPF.Behaviors
     /// </summary>
     public class ColumnHeaderBehavior : Behavior<DataGrid>
     {
-        #region DependencyProperty CellTemplate
-
-        public static readonly DependencyProperty CellTemplateProperty =
-            DependencyProperty.Register("CellTemplate",
-                                        typeof(DataTemplate),
-                                        typeof(ColumnHeaderBehavior),
-                                        new UIPropertyMetadata(null, CellTemplateChanged)
-                                        );
-
-        public DataTemplate CellTemplate
-        {
-            get
-            {
-                return (DataTemplate)GetValue(CellTemplateProperty);
-            }
-            set
-            {
-                SetValue(CellTemplateProperty, value);
-            }
-        }
-
-        private static void CellTemplateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            //
-        }
-
-        #endregion
-
-        #region DependencyProperty CellEditingTemplate
-
-        public static readonly DependencyProperty CellEditingTemplateProperty =
-            DependencyProperty.Register("CellEditingTemplate",
-                                        typeof(DataTemplate),
-                                        typeof(ColumnHeaderBehavior),
-                                        new UIPropertyMetadata(null, CellEditingTemplateChanged)
-                                        );
-
-        public DataTemplate CellEditingTemplate
-        {
-            get
-            {
-                return (DataTemplate)GetValue(CellEditingTemplateProperty);
-            }
-            set
-            {
-                SetValue(CellEditingTemplateProperty, value);
-            }
-        }
-
-        private static void CellEditingTemplateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            //
-        }
-
-        #endregion
-
         protected override void OnAttached()
         {
             AssociatedObject.AutoGeneratingColumn += (sender, e) =>
             {
                 var displayName = GetPropertyDisplayName(e.PropertyDescriptor);
-                if (!string.IsNullOrEmpty(displayName))
-                {
-                    var dataGridTemplateColumn = new DataGridTemplateColumn
-                                                 {
-                                                     CellTemplate = CellTemplate,
-                                                     CellEditingTemplate = CellEditingTemplate,
-                                                     Header = displayName
-                                                 };
-
-
-                    e.Column = dataGridTemplateColumn;
-
-
-                    //DataGrid grid = sender as DataGrid;
-                    //if (grid == null) { return; }
-                    
-                    //ObservableCollection<AutoGenerateColumn> coll = GetColumns(grid);
-
-                    //foreach (AutoGenerateColumn col in coll)
-                    //{
-                    //    if (e.PropertyName == col.Column)
-                    //    {
-                    //        CustomDataGridTemplateColumn templateColumn =
-                    //                new CustomDataGridTemplateColumn();
-                    //        templateColumn.Header = e.Column.Header;
-                    //        if (col.CellTemplate != null)
-                    //        {
-                    //            templateColumn.CellTemplate = col.CellTemplate;
-                    //        }
-                    //        if (col.CellEditingTemplate != null)
-                    //        {
-                    //            templateColumn.CellEditingTemplate = col.CellEditingTemplate;
-                    //        }
-                    //        if (col.Binding != null)
-                    //        {
-                    //            templateColumn.Binding = col.Binding;
-                    //        }
-
-                    //        templateColumn.SortMemberPath = e.Column.SortMemberPath;
-                    //        e.Column = templateColumn;
-                    //        return;
-                    //    }
-                    //}
-
-                    //return;
-
-
-
-
-
-                    e.Column.Header = displayName;
-                }
+                if (!string.IsNullOrEmpty(displayName)) e.Column.Header = displayName;
                 else e.Cancel = true;
 
                 var displayIndex = GetPropertyDisplayIndex(e.PropertyDescriptor);
