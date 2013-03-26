d3cc542570b1eaff183a83aaa43bb97bae482c9f
Shal1928 <Shal1928@gmail.com>
2013-03-26 08:59:47 +0000

    Add TypeBinding Attribute

---
+++ UseAbilities/UseAbilities.DotNet/Attributes/TypeBinding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UseAbilities.DotNet.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class TypeBinding : Attribute
+    {
+        public TypeBinding(Type type)
+        {
+            BindingWith = type;
+        }
+
+        public Type BindingWith
+        {
+            get;
+            private set;
+        }
+    }
+}
