0890a544fdc05eb72c61a46e5d9a89d4d71c1de1
Shal1928 <Shal1928@gmail.com>
2013-03-26 09:06:46 +0000

    Add TypeBindingExt

---
+++ UseAbilities/UseAbilities.DotNet/Attributes/Extensions/TypeBindingExt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace UseAbilities.DotNet.Attributes.Extensions
+{
+    public static class TypeBindingExt
+    {
+        public static Type BindingWith(this Enum enumValue)
+        {
+            return StringValueOf(enumValue, typeof(TypeBinding));
+        }
+
+        private static Type StringValueOf(IConvertible value, Type descriptionType)
+        {
+            var fi = value.GetType().GetField(value.ToString(CultureInfo.InvariantCulture));
+            var attributes = (TypeBinding[])fi.GetCustomAttributes(descriptionType, false);
+
+            if (attributes.Length > 0) return attributes[0].BindingWith;
+
+            throw new MissingMemberException(string.Format("{0} does not has attribute TypeBinding!", value));
+        }
+    }
+}
