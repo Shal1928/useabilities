d47b4bca8f24b7fbdf8066938a94b0048c407a6c
Shal1928 <Shal1928@gmail.com>
2013-06-06 10:23:55 +0000

    Fix name for BaseConverter

--- UseAbilities/UseAbilities.WPF/Converters/Test/TestConverter.cs
+++ UseAbilities/UseAbilities.WPF/Converters/Test/TestConverter.cs
@@ -4,9 +4,9 @@ using UseAbilities.WPF.Converters.Base;
 
 namespace UseAbilities.WPF.Converters.Test
 {
-    public class TestConverter : ConvertorBase<TestConverter>
+    public class TestConverter : ConverterBase<TestConverter>
     {
-        #region Ovveride of ConvertorBase
+        #region Ovveride of ConverterBase
 
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
