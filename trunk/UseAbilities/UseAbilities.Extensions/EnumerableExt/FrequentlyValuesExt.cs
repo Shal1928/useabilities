51bd474552dc479f855c5328d986f3b8f85ed231
Shal1928@gmail.com <Shal1928@gmail.com@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2013-06-17 08:37:46 +0000

    F: 1.0.9.0; Add FrequentlyValuesExt

---
+++ trunk/UseAbilities/UseAbilities.Extensions/EnumerableExt/FrequentlyValuesExt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace UseAbilities.Extensions.EnumerableExt
+{
+    public static class FrequentlyValuesExt
+    {
+        public static IEnumerable<int> GetFrequentlyValues(this IEnumerable<int> enumerable, int topCount)
+        {
+            return enumerable
+                   .GroupBy(i => i)
+                   .OrderByDescending(g => g.Count())
+                   .Take(topCount)
+                   .Select(g => g.Key);
+        }
+
+        //public static T GetFrequentlyValue<T>(this IEnumerable<T> enumerable)
+        //{
+        //    return enumerable
+        //           .GroupBy(i => i)
+        //           .OrderByDescending(g => g.Count())
+        //           .Take(1)
+        //           .Select(g => g.Key)
+        //           .FirstOrDefault();
+        //}
+    }
+}
