cc5175e371c37e5d5d4813353013d1b20ab62d06
Shal1928 <Shal1928@gmail.com>
2013-07-03 09:37:45 +0000

    F: 1.0.2.1; Add ValueChangedEventArgs;

---
+++ UseAbilities/UseAbilities.DotNet/EventArguments/ValueChangedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UseAbilities.DotNet.EventArguments
+{
+    public class ValueChangedEventArgs : EventArgs
+    {
+        public ValueChangedEventArgs(object oldValue, object newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+
+        public readonly object OldValue;
+        public readonly object NewValue;
+    }
+}
