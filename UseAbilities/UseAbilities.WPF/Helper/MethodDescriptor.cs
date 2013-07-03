222de3b784657e45c80bbbb937835f818b8a4f67
Shal1928 <Shal1928@gmail.com>
2013-07-03 04:05:15 +0000

    F: 1.0.5.0; Add PropertyChangedTrigger and MethodCallAction

---
+++ UseAbilities/UseAbilities.WPF/Helper/MethodDescriptor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+namespace UseAbilities.WPF.Helper
+{
+    public class MethodDescriptor
+    {
+        public MethodDescriptor(MethodInfo methodInfo, ParameterInfo[] methodParams)
+        {
+            MethodInfo = methodInfo;
+            Parameters = methodParams;
+        }
+
+        public bool HasParameters
+        {
+            get
+            {
+                return (Parameters.Length > 0);
+            }
+        }
+
+        public MethodInfo MethodInfo { get; private set; }
+
+        public int ParameterCount
+        {
+            get
+            {
+                return Parameters.Length;
+            }
+        }
+
+        public ParameterInfo[] Parameters { get; private set; }
+
+        public Type SecondParameterType
+        {
+            get
+            {
+                return Parameters.Length >= 2 ? Parameters[1].ParameterType : null;
+            }
+        }
+    }
+}
