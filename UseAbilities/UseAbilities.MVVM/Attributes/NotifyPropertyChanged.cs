e4ba07ce798f85960882fa2f14cd7234db24a825
Shal1928 <Shal1928@gmail.com>
2013-07-10 08:36:23 +0000

    F: 1.1.0.0; Add ObserveWrapper; OnPorpertyChanged(string) now is public

---
+++ UseAbilities/UseAbilities.MVVM/Attributes/NotifyPropertyChanged.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UseAbilities.MVVM.Attributes
+{
+    //TODO: Delete Not Used
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NotifyPropertyChanged : Attribute
+    {
+        public NotifyPropertyChanged()
+        {
+            //
+        }
+    }
+}
