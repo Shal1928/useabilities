b82b8e11c55d514d5023e17fe648a472eb78d84f
Shal1928 <Shal1928@gmail.com>
2013-07-10 05:54:19 +0000

    F: 1.0.2.0; Fix IoC for work with ObserveWrapper (PropertyChanged PROXY)

--- UseAbilities/UseAbilities.IoC/Core/RegistredObject.cs
+++ UseAbilities/UseAbilities.IoC/Core/RegistredObject.cs
@@ -7,6 +7,18 @@ using UseAbilities.IoC.Extensions;
 
 namespace UseAbilities.IoC.Core
 {
+    public static class Extensions
+    {
+        public static MethodInfo GetSetMethodOnDeclaringType(this PropertyInfo propertyInfo)
+        {
+            var methodInfo = propertyInfo.GetSetMethod(true);
+            return methodInfo ?? propertyInfo
+                                    .DeclaringType
+                                    .GetProperty(propertyInfo.Name)
+                                    .GetSetMethod(true);
+        }
+    }
+
     internal class RegisteredObject
     {
         private readonly bool _isSinglton;
@@ -20,13 +32,13 @@ namespace UseAbilities.IoC.Core
 
         public Type ConcreteType
         {
-            get; 
+            get;
             private set;
         }
 
         public object Instance
         {
-            get; 
+            get;
             private set;
         }
 
@@ -34,13 +46,39 @@ namespace UseAbilities.IoC.Core
         {
             var newExp = Expression.New(ConcreteType);
             var dictParam = Expression.Parameter(typeof(Dictionary<PropertyInfo, object>), "d");
-            var memberBindingList = (from propertyInfo in propertiesMappingDic.Keys
-                                     let call = Expression.Call(typeof (DictionaryExtensions), 
-                                                                "GetValue", 
-                                                                new[]{propertyInfo.PropertyType}, 
-                                                                new Expression[]{dictParam, Expression.Constant(propertyInfo)})//let call
-                                     select Expression.Bind(propertyInfo.GetSetMethod(), call)).Cast<MemberBinding>().ToList();
+            //var memberBindingList = (from propertyInfo in propertiesMappingDic.Keys
+            //                         let call = Expression.Call(typeof (DictionaryExtensions), 
+            //                                                    "GetValue", 
+            //                                                    new[]{propertyInfo.PropertyType}, 
+            //                                                    new Expression[]{dictParam, Expression.Constant(propertyInfo)})//let call
+            //                         select Expression.Bind(propertyInfo.GetSetMethod(), call)).Cast<MemberBinding>().ToList();
 
+
+            //var memberBindingList = (from propertyInfo in propertiesMappingDic.Keys let call = Expression.Call(typeof(DictionaryExtensions), "GetValue", new[] { propertyInfo.PropertyType }, new Expression[] { dictParam, Expression.Constant(propertyInfo) })//let call
+            //                         select Expression.Bind(propertyInfo.GetSetMethod(), call)).Cast<MemberBinding>().ToList();
+
+            //For ObserveWrapper
+            //var memberBindingList = new List<MemberBinding>();
+
+            //foreach (var propertyInfo in propertiesMappingDic.Keys)
+            //{
+            //    var expression = new Expression[] { dictParam, Expression.Constant(propertyInfo) };
+            //    var array = new[] { propertyInfo.PropertyType };
+
+            //    var call = Expression.Call(typeof(DictionaryExtensions), "GetValue", array, expression);
+            //    //var setMethod = propertyInfo.GetSetMethod();
+
+            //    var memberBinding = Expression.Bind(propertyInfo, call);
+            //    memberBindingList.Add(memberBinding);
+            //}
+
+            var memberBindingList = (from propertyInfo in propertiesMappingDic.Keys
+                                     let call = Expression.Call(typeof(DictionaryExtensions),
+                                                                "GetValue",
+                                                                new[] { propertyInfo.PropertyType },
+                                                                new Expression[] { dictParam, Expression.Constant(propertyInfo) })//let call
+                                     select Expression.Bind(propertyInfo, call)).Cast<MemberBinding>().ToList();
+            
             var lambda = Expression.Lambda<Func<Dictionary<PropertyInfo, object>, object>>(Expression.MemberInit(newExp, memberBindingList), new[] { dictParam });
 
             var compiled = lambda.Compile();
