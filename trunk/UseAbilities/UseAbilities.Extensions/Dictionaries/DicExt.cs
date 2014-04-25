4c06fa5b9c2395ae8664f272b0369292a9c37924
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2014-04-25 07:50:13 +0000

    Добавлено расширение для Dictionaries с методом Put(key,value)

---
+++ trunk/UseAbilities/UseAbilities.Extensions/Dictionaries/DicExt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace UseAbilities.Extensions.Dictionaries
+{
+    public static class DicExt
+    {
+        public static void Put<TKey, TVal>(this Dictionary<TKey, TVal> dic, TKey key, TVal value)
+        {
+            if (dic.ContainsKey(key)) dic[key] = value;
+            else dic.Add(key, value);
+        }
+    }
+}
