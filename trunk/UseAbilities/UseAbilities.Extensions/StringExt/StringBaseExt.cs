66ab603e6954ca866cdbfd5cb3c118f26028e540
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2014-05-01 07:21:54 +0000

    Add String Base extensions

--- trunk/UseAbilities/UseAbilities.Extensions/StringExt/StringBaseExt.cs
+++ trunk/UseAbilities/UseAbilities.Extensions/StringExt/StringBaseExt.cs
@@ -5,9 +5,42 @@ namespace UseAbilities.Extensions.StringExt
 {
     public static class StringBaseExt
     {
+        public static readonly string NEW_LINE = "\n";
+
         public static bool IsNullOrEmptyOrSpaces(this string value)
         {
             return String.IsNullOrWhiteSpace(value) || value.IsEmpty();
+        }
+
+        public static string Copy(this string value, int beginIndex, int endIndex)
+        {
+            return value.Substring(beginIndex, endIndex - beginIndex + 1);
+        }
+
+        public static string RemoveLast(this string value)
+        {
+            return value.Remove(value.IndexOfLast());
+        }
+
+        public static string GetAfterNewLine(this string value)
+        {
+            var newLineIndex = value.IndexOfNext(NEW_LINE);
+            return value.Copy(newLineIndex, value.IndexOfLast());
+        }
+
+        public static int IndexOfNext(this string value, string item, StringComparison comparison = StringComparison.Ordinal)
+        {
+            return value.IndexOf(item, comparison) + 1;
+        }
+
+        public static int IndexOfPreview(this string value, string item, StringComparison comparison = StringComparison.Ordinal)
+        {
+            return value.IndexOf(item, comparison) - 1;
+        }
+
+        public static int IndexOfLast(this string value)
+        {
+            return value.Length - 1;
         }
     }
 }
