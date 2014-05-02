a17ef443664c1ba1e10576c38648485feab6a183
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2014-05-02 09:03:21 +0000

    Add DivideDoubleValueConverter

---
+++ UseAbilities/UseAbilities.WPF/Converters/DivideDoubleValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using UseAbilities.WPF.Converters.Base;
+
+namespace UseAbilities.WPF.Converters
+{
+    [ValueConversion(typeof(double), typeof(double))]
+    public class DivideDoubleValueConverter : ConverterBase<DivideDoubleValueConverter>
+    {
+        public DivideDoubleValueConverter()
+        {
+            //
+        }
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is double)) throw new ArgumentException("Value is not double!");
+            var doubleValue = (double)value;
+            return doubleValue / 2;
+        }
+    }
+}
