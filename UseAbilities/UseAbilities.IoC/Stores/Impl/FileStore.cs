804596fc023eff4da8f12adf9126cddb0969d76d
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2014-05-03 07:01:28 +0000

    Унифицировал интерфейсы хранилищ, реализовал хранилище файловой системы, эксэмель хранилище

---
+++ UseAbilities/UseAbilities.IoC/Stores/Impl/FileStore.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+
+namespace UseAbilities.IoC.Stores.Impl
+{
+    public class FileStore : AFileStore<byte[]>
+    {
+        public override string FileName { get; set; }
+
+        public override byte[] Load()
+        {
+            return Load(FileName);
+        }
+
+        public override byte[] Load(string fileName)
+        {
+            if (!File.Exists(fileName)) throw new FileNotFoundException(fileName);
+
+            byte[] loadObject;
+            using (var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                var bytes = new byte[fileStream.Length];
+                var numBytesToRead = (int)fileStream.Length;
+                var numBytesRead = 0;
+
+                while (numBytesToRead > 0)
+                {
+                    var n = fileStream.Read(bytes, numBytesRead, numBytesToRead);
+                    if (n == 0) break;
+
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+                }
+
+                loadObject = bytes;
+            }
+
+            return loadObject;
+        }
+
+        public override void Save(byte[] storeObject)
+        {
+            Save(storeObject, FileName);
+        }
+
+        public override void Save(byte[] storeObject, string fileName)
+        {
+            using (var fileStream = new FileStream(fileName, FileMode.Create))
+            {
+                fileStream.Write(storeObject, 0, storeObject.Length);
+            }
+        }
+    }
+}
