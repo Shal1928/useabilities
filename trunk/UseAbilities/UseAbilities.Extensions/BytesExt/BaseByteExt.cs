e6832215c33489de8ffcc12adeb8f3d0e78e8c6d
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2014-05-03 09:45:13 +0000

    Add BaseByteExt

---
+++ trunk/UseAbilities/UseAbilities.Extensions/BytesExt/BaseByteExt.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace UseAbilities.Extensions.BytesExt
+{
+    public static class BaseByteExt
+    {
+        public static string GetStringUTF8(this byte[] bytes)
+        {
+            return bytes.GetString(Encoding.UTF8);
+        }
+
+        public static string GetString(this byte[] bytes, Encoding encoding)
+        {
+            return encoding.GetString(bytes);
+        }
+    }
+}
