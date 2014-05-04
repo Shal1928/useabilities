e2a15cf30edf534d82fb05b17e3758c21c703300
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2014-05-04 08:39:44 +0000

    Add Ext method to object for getValue from property by string name

--- UseAbilities/UseAbilities.Extensions/ObjectExt/ObjectBaseExt.cs
+++ UseAbilities/UseAbilities.Extensions/ObjectExt/ObjectBaseExt.cs
@@ -1,4 +1,10 @@
-﻿namespace UseAbilities.Extensions.ObjectExt
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq.Expressions;
+using UseAbilities.Extensions.StringExt;
+
+namespace UseAbilities.Extensions.ObjectExt
 {
     public static class ObjectBaseExt
     {
@@ -12,11 +18,21 @@
             return obj != null;
         }
 
-        //public static T GetValue<T>(this object obj, string propertyName)
+        public static T GetValue<T>(this object obj, string propertyName)
+        {
+            if(propertyName.IsNullOrEmptyOrSpaces()) throw new InvalidDataException("Property name is invalid!");
+            var info = obj.GetType().GetProperty(propertyName);
+            return (T)info.GetValue(obj, null);
+        }
+
+        //public static T GetValue<T>(this object obj, Expression<Func<T>> propertySelector)
         //{
-        //    var info = obj.GetType().GetProperty(propertyName);
-        //    return (T)info.GetValue(obj, null);
+        //    var memberExpression = propertySelector.Body as MemberExpression;
+        //    if (memberExpression == null) throw new InvalidExpressionException("PropertySelector is null or not MemberExpression!");
+
+        //    return obj.GetValue<T>(memberExpression.Member.Name);
         //}
+
 
         //TODO: Make with Expression safe call Props or Meths with null check
         //public static bool SafeCheck(this object obj, )
