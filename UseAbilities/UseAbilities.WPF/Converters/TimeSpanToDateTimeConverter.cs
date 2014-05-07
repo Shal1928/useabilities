e35babf517759c5afc2bb99a62c36353d063db1c
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2014-05-07 07:45:02 +0000

    Add TimeSpanToDateTimeConverter, DateTimeToTimeSpanConverter

---
+++ UseAbilities/UseAbilities.WPF/Converters/TimeSpanToDateTimeConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using UseAbilities.WPF.Converters.Base;
+
+namespace UseAbilities.WPF.Converters
+{
+    [ValueConversion(typeof(TimeSpan), typeof(DateTime))]
+    public class TimeSpanToDateTimeConverter : ConverterBase<TimeSpanToDateTimeConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is DateTime)) throw new ArgumentException("value is not DateTime");
+            var dateTimeValue = (DateTime)value;
+            return new TimeSpan(0, dateTimeValue.Hour, dateTimeValue.Minute, dateTimeValue.Second, dateTimeValue.Millisecond);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is TimeSpan)) throw new ArgumentException("value is not TimeSpan");
+            var timeSpanValue = (TimeSpan)value;
+            return new DateTime(1,1,1, timeSpanValue.Hours, timeSpanValue.Minutes, timeSpanValue.Seconds, timeSpanValue.Milliseconds);
+        }
+    }
+
+    [ValueConversion(typeof(DateTime), typeof(TimeSpan))]
+    public class DateTimeTimeSpanConverter : ConverterBase<DateTimeTimeSpanConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is TimeSpan)) throw new ArgumentException("value is not TimeSpan");
+            var timeSpanValue = (TimeSpan)value;
+            return new DateTime(1, 1, 1, timeSpanValue.Hours, timeSpanValue.Minutes, timeSpanValue.Seconds, timeSpanValue.Milliseconds);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is DateTime)) throw new ArgumentException("value is not DateTime");
+            var dateTimeValue = (DateTime)value;
+            return new TimeSpan(0, dateTimeValue.Hour, dateTimeValue.Minute, dateTimeValue.Second, dateTimeValue.Millisecond);
+        }
+    }
+}
