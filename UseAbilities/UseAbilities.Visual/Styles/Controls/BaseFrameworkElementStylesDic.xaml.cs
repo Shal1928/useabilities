3fc704791c03302f6d0d2e291d9c715cfae00f29
Shal1928 <Shal1928@fc395a96-6cf3-bceb-9e92-0b2378fbf215>
2014-05-08 09:04:15 +0000

    Add Drafts Styles

---
+++ UseAbilities/UseAbilities.Visual/Styles/Controls/BaseFrameworkElementStylesDic.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace UseAbilities.Visual.Styles.Controls
+{
+    /// <summary>
+    /// Interaction logic for BaseFrameworkElementStylesDic.xaml
+    /// </summary>
+    public partial class BaseFrameworkElementStylesDic
+    {
+        public BaseFrameworkElementStylesDic()
+        {
+            InitializeComponent();
+        }
+    }
+}
